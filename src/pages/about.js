2237d8c682d7db115dd00169bd9a9621b869af03
Brad Root <brad@thenounproject.com>
2019-02-25 16:40:54 -0800

    don't nest a ul inside a p

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,13 +15,13 @@ const AboutPage = props => {
             <p>In my free time, I develop <a href="https://itunes.apple.com/us/app/numu-new-music-tracker/id1158641228?mt=8">Numu Tracker</a>, a FOSS project that basically acts as a automatic "to-do list" for music you want to listen to. You can follow along as I build out the new verison, or contribute, <a href="https://github.com/numutracker">over on GitHub</a>.</p>
             <p>
               Here's some ways you can find or reach me:
-              <ul>
-                <li><a href="https://www.twitter.com/amiantos">Twitter</a></li>
-                <li><a href="https://github.com/amiantos">GitHub</a></li>
-                <li><a href="https://www.linkedin.com/in/amiantos/">LinkedIn</a></li>
-                <li><a href="mailto:amiantos@icloud.com">Email</a></li>
-              </ul>
             </p>
+            <ul>
+              <li><a href="https://www.twitter.com/amiantos">Twitter</a></li>
+              <li><a href="https://github.com/amiantos">GitHub</a></li>
+              <li><a href="https://www.linkedin.com/in/amiantos/">LinkedIn</a></li>
+              <li><a href="mailto:amiantos@icloud.com">Email</a></li>
+            </ul>
             <p>This site was built using GatsbyJS, a static site generator for React. It's pretty cool. You can view the source code for this site on <a href="https://www.github.com/amiantos/amiantos.net">GitHub</a>.</p>
           </Content>
         </Post>
